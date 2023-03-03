dd087a72b27817b978b8ec13f38316c04c5f5ff3
giddie-ui <giddiekoech6@gmail.com>
2023-03-03 12:27:40 +0300

    cool

---
+++ app/src/main/java/com/example/farmgenix/breed.kt
@@ -0,0 +1,11 @@
+package com.example.farmgenix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class breed : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_breed)
+    }
+}
